31250e5493e6cb0c0fec7c80a796a78c138d691c
Jose Alberto <ja@josealberto.org>
2015-01-09 16:43:32 +0000

    add recipe to populate authorized_keys from github

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,10 @@
+node.default['securize']['deploy_user'] = 'deploy'
 node.default['securize']['ufw'] = {
   'ips_whitelist'    => [],
   'ports_whitelist'  => %w(22),
   'default_incoming' => "reject"
+}
+node.default['securize']['sshkeys'] = {
+  'gh_url'   => 'https://github.com',
+  'gh_users' => %w()
 }
---
+++ recipes/sshkeys.rb
@@ -0,0 +1,42 @@
+gh_users             = node['securize']['sshkeys']['gh_users']
+keys_file            = "#{Dir.mktmpdir}/gh.keys"
+authorized_keys_file = "/home/#{node['securize']['deploy_user']}/.ssh/authorized_keys"
+
+unless gh_users.empty?
+
+  Array(gh_users).each do |user|
+    remote_file keys_file do
+      source "#{node['securize']['sshkeys']['gh_url']}/#{user}.keys"
+    end
+
+    ruby_block "add_key" do
+      block do
+        last_key = IO.readlines(keys_file)[-1..-1].first
+        if last_key
+          file = Chef::Util::FileEdit.new(authorized_keys_file)
+          file.insert_line_if_no_match(/#{last_key}/, last_key)
+          file.write_file
+        end
+      end
+      notifies :run, "ruby_block[remove_duplicates]", :immediately
+      only_if { File.exists?(authorized_keys_file) }
+    end
+
+    ruby_block "remove_duplicates" do
+      block do
+        # TODO: must be a better way
+        uniq_keys = File.readlines(authorized_keys_file).uniq
+        FileUtils.cp(authorized_keys_file, authorized_keys_file + ".bak")
+        File.open(authorized_keys_file, "w+") { |f| f.puts uniq_keys  }
+      end
+      action :nothing # This run only if add_key is succeful
+    end
+  end
+
+else
+  log "gh_users empty" do
+    level :warn
+    message "gh_users key is empty, please add some Github users to populate"
+  end
+end
+
